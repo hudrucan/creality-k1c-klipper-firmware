7942d8b5af1f44c065b886a3bb05d0e487f68b44
hudrucan <huy.nguyen@employmenthero.com>
2024-10-31 12:17:23 +0700

    Tweak moving Z before going to next probe point

--- klippy/extras/axis_twist_compensation.py
+++ klippy/extras/axis_twist_compensation.py
@@ -224,6 +224,10 @@ class Calibrater:
             else:
                 # move to next point
                 self.current_point_index += 1
+
+                # horizontal_move_z (to prevent probe trigger or hitting bed)
+                self._move_helper((None, None, self.horizontal_move_z))
+
                 self._calibration(probe_points, nozzle_points, interval)
         return callback
 
